100a25d9fd76fdb27005ce96185ba6d7c7474230
antagomir <leo.lahti@iki.fi>
2018-05-19 23:44:54 +0200

    Site store

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ jekyll build
 git commit -a -m"Site store"
 # Run the next line the first time when setting up all
 git push origin 'git subtree split --prefix _site devel':master --force
-git subtree push --prefix _site/* origin master
+git subtree push --prefix _site origin master
 git push origin devel
 
 
